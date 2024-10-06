fe258fec8b78c377fc109a3efd559efeb9dddd8d
Garry <tahinjanaharygarry@gmail.com>
2024-10-06 15:52:09 +0300

    config eslint and jest

--- appointments/eslint.config.mjs
+++ appointments/eslint.config.mjs
@@ -2,10 +2,20 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    plugins: ["jest"],
+    env: { "jest/globals": true },
+    rules: {
+      "jest/no-disabled-tests": "warn",
+      "jest/no-focused-tests": "error",
+      "jest/no-identical-title": "error",
+      "jest/prefer-to-have-length": "warn",
+      "jest/valid-expect": "error",
+    },
+  },
 ];
